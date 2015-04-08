ab5cd0a41e8a2962c373b7d8a5adc8ef10b0513a
Yarden ARANE <yarden.arane@mail.mcgill.ca>
2015-04-08 18:20:36 -0400

    cleaned up dir.

--- cgi/login.c
+++ cgi/login.c
@@ -82,7 +82,7 @@ int main(){
 	parseCredentials(string, length, username, password);
 
 	if(validateCredentials(username, password)){
-		printf("\t\t<center><h1>LOGIN SUCCESSFUL</h1></center>\n\t\t<p>Welcome back, %s.</p>\n\t\t<form action=\"MyFacebookPage.py\" method=post>\n\t\t\t<input type=\"hidden\" name=\"username\" value=\"%s\">\n\t\t\t<input type=\"submit\" value=\"Proceed to feed\">\n\t\t</form>", username);	
+		printf("\t\t<center><h1>LOGIN SUCCESSFUL</h1></center>\n\t\t<p>Welcome back, %s.</p>\n\t\t<form action=\"MyFacebookPage.py\" method=post>\n\t\t\t<input type=\"hidden\" name=\"username\" value=%s>\n\t\t\t<input type=\"submit\" value=\"Proceed to feed\">\n\t\t</form>", username, username);	
 	}else{
 		printf("\t\t<center><h1>LOGIN UNSUCCESSFUL</h1></center>\n\t\t<p><a href=\"index.html\"><i>Back to homepage</i></a></p>\n");
 	}
@@ -91,3 +91,4 @@ int main(){
 	printf("\t</body>\n</html>");
 	return 0;	
 }
+
