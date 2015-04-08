3a10c34c5fb36fd293c4343f1353cca27ca81671
Brett Leighton <me@brettleighton.com>
2015-04-08 19:06:37 -0400

    feed changes

--- cgi/MyFacebookPage.py
+++ cgi/MyFacebookPage.py
@@ -46,22 +46,16 @@ def main():
   posts = readFeed(username)
   members = retrieveUsernames()
   print "Content-type:text/html\r\n\r\n"
-  print '<html>'
-  print '<head>'
-  print "<title>Hey there %s! Welcome to your feed</title>" % (username)
-  print '</head>'
-  print '<body>'
-  print "<p> USER: %s </p>" % (username)
+  print "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>FEED</title>\n\t</head>\n\t<body>\n\n\t\t<center><h1>FEED</h1>\n\t\t<p><i>Logged in as: %s</i></p><br>\n\t\t<form action\"createNewPost.py\" method=post>\n\t\t\tNew post:<br>\n\t\t\t<input type=\"text\" name=\"post\">\n\t\t\t<input type=\"submit\" value=\"Submit\">\n\t\t</form><br><br>\n\t\t<p>" % (username)
+  
   for post in posts:
-    print "<p>%s posted -> %s</p>" % (post[0], post[1])
-  print "<p>The existing members are the following:</p>"
+    print "\t\t\t&emsp;&emsp;%s:&emsp;%s\n" % (post[0], post[1])
+
+  print "\t\t</p></center>\n\t\t<p>User list:<br>\n\t\t\t"
   for member in members:
-    print "<p>%s</p>" % (member)
-  print "<form action=\"addNewFriend.py\" method=post><fieldset><input type=\"hidden\" name=\"username\" value=\"%s\"><br><br>Add new Friend:<br><input type=\"text\" name=\"friendUsername\" required><br><br><input type=\"submit\" value=\"Add Friend\"></fieldset></form>" % (username)
-  print "<form action=\"createNewPost.py\" method=post><fieldset><input type=\"hidden\" name=\"username\" value=\"%s\"><br><br>Create a new status update:<br><input type=\"text\" name=\"postData\" required><br><br><input type=\"submit\" value=\"Submit\"></fieldset></form>" % (username)
-  print "<a href=\"../index.html\">Logout</a>"
-  print '</body>'
-  print '</html>'
+    print "\t\t\t&emsp;%s\n" % (member)
+
+  print "\t\t<p>Don't have a social life? Want to follow people just like you? Add a user below:</p>\n\t\t<form action=\"addNewFriend.py\" method=post>\n\t\t\t&emsp;Username:<br>\n\t\t\t&emsp;<input type=\"text\" name=\"username\">\n\t\t\t<input type=\"submit\" value=\"Follow\">\n\t\t\t</fieldset>\n\t\t\t<br>\n\t\t</form>\n\t</body>\n</html>"
 
 main()
 
