0b23e237f3384f3c7c2414471a7709e07bc46afb
Brett Leighton <me@brettleighton.com>
2015-04-08 22:20:05 -0400

    100% of the assignment

--- cgi/MyFacebookPage.py
+++ cgi/MyFacebookPage.py
@@ -49,12 +49,12 @@ def main():
   print "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>FEED</title>\n\t</head>\n\t<body>\n\n\t\t<center><h1>FEED</h1>\n\t\t<p><i>Logged in as: %s</i></p><br>\n\t\t<form action=\"createNewPost.py\" method=post>\n\t\t\t<input type=\"hidden\" name=\"username\" value=\"%s\">\n\t\t\tNew post:<br>\n\t\t\t<input type=\"text\" name=\"postData\">\n\t\t\t<input type=\"submit\" value=\"Post\">\n\t\t</form></center><br><br>\n\t\t" % (username, username)
   
   for post in posts:
-    print "\t\t\t<p>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>%s</b>:&emsp;&emsp;&emsp;<i>%s</i></p>" % (post[0], post[1])
+    print "\t\t\t<p>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b>%s: </b><i>%s</i></p>" % (post[0], post[1])
 
-  print "\t\t</p>\n\t\t<h3>User list:</h3><br>\n\t\t\t<ul>\n\t\t\t\t"
+  print "\t\t<br></p>\n\t\t<h3>User list:</h3>\n\t\t\t<ul>\n\t\t\t\t"
   for member in members:
     print "\t\t\t\t<li>%s</li>\n" % (member)
 
-  print "\t\t\t</ul><br><br>\n\t\t<p>Don't have a social life? Want to follow people just like you? Add a user below:</p>\n\t\t<form action=\"addNewFriend.py\" method=post>\n\t\t\t<input type=\"hidden\" name=\"username\" value=\"%s\">\n\t\t\t&emsp;Username:<br>\n\t\t\t&emsp;<input type=\"text\" name=\"friendUsername\">\n\t\t\t<input type=\"submit\" value=\"Follow\">\n\t\t\t</fieldset>\n\t\t\t<br>\n\t\t</form>\n\t<p><i><a href=\"../index.html\">Logout</a></i></p>\n\t</body>\n</html>" % (username)
+  print "\t\t\t</ul><br><br>\n\t\t<form action=\"addNewFriend.py\" method=post>\n\t\t\t<input type=\"hidden\" name=\"username\" value=\"%s\">\n\t\t\t<fieldset><h3>Don't have a social life? Want to follow people just like you? Add a user below:</h3>\n\t\t\t&emsp;Username:<br>\n\t\t\t&emsp;<input type=\"text\" name=\"friendUsername\">\n\t\t\t<input type=\"submit\" value=\"Follow\">\n\t\t\t</fieldset>\n\t\t\t<br>\n\t\t</form>\n\t<p><i><a href=\"../index.html\">Logout</a></i></p>\n\t</body>\n</html>" % (username)
 
 main()
